a65110185d6db887f1906ebd5b45ca44c3183bc9
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-10 11:55:54 -0400

    Create SignatureWidget.php

---
+++ src/Widget/SignatureWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    brightcloudstudio/contao-signature-form-field
+ * @license    GPL-3.0-or-later
+ * @link       https://github.com/brightcloudstudio/contao-signature-form-field
+ */
+
+declare(strict_types=1);
+
+namespace BCS\ContaoSignatureFormField\Widget;
+
+use Contao\Widget;
+
+class SignatureWidget extends Widget
+{
+    protected $strTemplate = 'form_signature';
+
+    protected $blnForAttribute = false;
+
+    public static $arrAttributes = array();
+
+    public function generate(): string
+    {
+        return $this->inherit();
+    }
+
+    protected function validator($varInput): mixed
+    {
+        return $varInput;
+    }
+}
