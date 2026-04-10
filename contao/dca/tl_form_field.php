65b7be711db9675f58303d00914e76902ed88368
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-10 11:57:41 -0400

    Create tl_form_field.php

---
+++ contao/dca/tl_form_field.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    brightcloudstudio/contao-signature-form-field
+ * @license    GPL-3.0-or-later
+ * @link       https://github.com/brightcloudstudio/contao-signature-form-field
+ */
+
+use BCS\ContaoSignatureFormField\Widget\SignatureWidget;
+
+$GLOBALS['TL_FFL']['signature'] = SignatureWidget::class;
