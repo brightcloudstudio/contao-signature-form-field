808a17752e7c6a938510cd3615972e493a6b3657
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-10 11:58:21 -0400

    Create default.php

---
+++ contao/languages/en/default.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    brightcloudstudio/contao-signature-form-field
+ * @license    GPL-3.0-or-later
+ * @link       https://github.com/brightcloudstudio/contao-signature-form-field
+ */
+
+$GLOBALS['TL_LANG']['FFL']['signature'] = array('Signature', 'Adds a signature pad field to the form.');
