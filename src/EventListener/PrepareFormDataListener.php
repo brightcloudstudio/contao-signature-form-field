37e3f8a00d1b2fa77f4df55dcb6a942d17555fa6
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-04-13 14:38:28 -0400

    Update PrepareFormDataListener.php

--- src/EventListener/PrepareFormDataListener.php
+++ src/EventListener/PrepareFormDataListener.php
@@ -19,78 +19,103 @@ use Dompdf\Options;
 #[AsHook('prepareFormData')]
 class PrepareFormDataListener
 {
+    /**
+     * The Contao project root directory, injected via the constructor.
+     * In services.yaml bind "%kernel.project_dir%" to $projectDir.
+     */
+    public function __construct(private readonly string $projectDir)
+    {
+    }
+
     public function __invoke(array &$submittedData, array $labels, array $fields, Form $form): void
     {
-
-
-        // Initialize DomPDF stuffs
+        // Initialize DomPDF
         $options = new Options();
         $options->set("defaultFont", "Times-Roman");
         $options->set("isRemoteEnabled", "true");
         $options->setChroot('/');
-    	$dompdf = new Dompdf($options);
-    	$context = stream_context_create([ 
-        	'ssl' => [ 
-        		'verify_peer' => FALSE, 
-        		'verify_peer_name' => FALSE,
-        		'allow_self_signed'=> TRUE 
-        	] 
+        $dompdf = new Dompdf($options);
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer'       => FALSE,
+                'verify_peer_name'  => FALSE,
+                'allow_self_signed' => TRUE,
+            ]
         ]);
         $dompdf->setHttpContext($context);
 
 
-
         // Get our template HTML
-        //$html = file_get_contents('../templates/volunteer_coi_form.html', true);
         $html = file_get_contents('bundles/contaosignatureformfield/templates/volunteer_coi_form.html', true);
 
 
         // Swap out template tags
         preg_match_all('/\{{2}(.*?)\}{2}/is', $html, $tags);
-        foreach($tags[0] as $tag) {
-            
+        foreach ($tags[0] as $tag) {
+
             // Remove brackets from our tag
-            $cleanTag = str_replace("{{","",$tag);
-            $cleanTag = str_replace("}}","",$cleanTag);
-            
-    	    $explodedTag = explode("::", $cleanTag);
-    	    
-    	    // Do different things based on the first part of our tag
-    	    switch($explodedTag[0]) {
-    		    
-    		    case 'member':
-    		        switch($explodedTag[1]) {
-    		            case 'name':
-    		                $html = str_replace($tag, $member['name'], $html);
-    		                break;
-    		        }
-    		    break;
-    	    }
+            $cleanTag = str_replace("{{", "", $tag);
+            $cleanTag = str_replace("}}", "", $cleanTag);
+
+            $explodedTag = explode("::", $cleanTag);
+
+            // Do different things based on the first part of our tag
+            switch ($explodedTag[0]) {
+
+                case 'member':
+                    switch ($explodedTag[1]) {
+                        case 'name':
+                            $html = str_replace($tag, $member['name'], $html);
+                            break;
+                    }
+                    break;
+            }
         }
 
 
         // Load our HTML into dompdf
-    	$dompdf->loadHtml($html);
-    	
-    	// Set our paper size and orientation
-    	$dompdf->setPaper('A4', 'landscape');
-    	
-    	// Render our PDF using the loaded HTML
-    	$dompdf->render();
-    	
-    	// Output the generated PDF to Browser
-    	$dompdf->stream("certificate.pdf");
+        $dompdf->loadHtml($html);
+
+        // Set our paper size and orientation
+        $dompdf->setPaper('A4', 'landscape');
+
+        // Render our PDF using the loaded HTML
+        $dompdf->render();
 
 
-        
+        // ------------------------------------------------------------------
+        // Save the PDF to public_html/files/content/volunteer_coi
+        // ------------------------------------------------------------------
+
+        // Build the absolute path to the save directory.
+        // $this->projectDir is the Contao project root (one level above public/).
+        // Adjust the path below if your web root folder is named differently
+        // (e.g. "web" instead of "public_html").
+        $saveDir = $this->projectDir . '/public_html/files/content/volunteer_coi';
+
+        // Create the directory if it does not already exist
+        if (!is_dir($saveDir)) {
+            mkdir($saveDir, 0755, true);
+        }
+
+        // Build a unique filename using a timestamp + random suffix
+        $filename  = 'volunteer_coi_' . date('Ymd_His') . '_' . bin2hex(random_bytes(4)) . '.pdf';
+        $filePath  = $saveDir . '/' . $filename;
+
+        // Write the PDF bytes to disk instead of streaming to the browser
+        $pdfContent = $dompdf->output();
+        file_put_contents($filePath, $pdfContent);
+
+        // Relative path from the web root – handy for storing as a URL or in Leads
+        $relativeUrl = 'files/content/volunteer_coi/' . $filename;
+        // ------------------------------------------------------------------
+
+
         $submittedData['first_name'] = 'Test';
 
-        // Generate PDF
-
-        // Store PDF
-        $submittedData['generated_pdf'] = 'https://www.youtube.com/watch?v=1O2d5yoLsok';
+        // Store the server-relative path to the saved PDF
+        $submittedData['generated_pdf'] = $relativeUrl;
 
         // Add PDF URL to hidden field on form so it gets stored in Leads
-     
     }
 }
